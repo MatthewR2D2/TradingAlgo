8d8ead0ff04a41b2fbcf7f3013a0a02719118548
Matthew <matthew.c.r.millar@gmail.com>
2019-02-18 10:58:05 +0900

    More Modular and cleaner

--- FintechTest.py
+++ FintechTest.py
@@ -21,6 +21,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from TradingAlgoUtilities import DataExplorationUtils as DEU
+from TestingUtilities import EvaluationTools as EveT
+from TestingUtilities import BacktestingAlgo as BTA
 
 
 instrument = "WIKI/AAPL" # Quandl Code for each stock
@@ -172,67 +174,19 @@ ax1.plot(signals.loc[signals.positions == -1.0].index,
 # Show the plot
 #plt.show()
 
+
+
 '''
 Backtesting the simple algo
 '''
-
 initialCapital = float(100000.0)
-
-# Create a DataFrame `positions`
-positions = pd.DataFrame(index=signals.index).fillna(0.0)
-
-# Buy a 100 shares
-positions['AAPL'] = 100*signals['signal']
-
-# Initialize the portfolio with value owned
-portfolio = positions.multiply(aapl['Adj. Close'], axis=0)
-
-# Store the difference in shares owned
-pos_diff = positions.diff()
-
-# Add `holdings` to portfolio
-portfolio['holdings'] = (positions.multiply(aapl['Adj. Close'], axis=0)).sum(axis=1)
-
-# Add `cash` to portfolio
-portfolio['cash'] = initialCapital - (pos_diff.multiply(aapl['Adj. Close'], axis=0)).sum(axis=1).cumsum()
-
-# Add `total` to portfolio
-portfolio['total'] = portfolio['cash'] + portfolio['holdings']
-
-# Add `returns` to portfolio
-portfolio['returns'] = portfolio['total'].pct_change()
-
-# Print the first lines of `portfolio`
-print(portfolio.head())
-
-
-# Create a figure to show the back testing
-fig = plt.figure()
-
-ax1 = fig.add_subplot(111, ylabel='Portfolio value in $')
-
-# Plot the equity curve in dollars
-portfolio['total'].plot(ax=ax1, lw=2.)
-
-ax1.plot(portfolio.loc[signals.positions == 1.0].index,
-         portfolio.total[signals.positions == 1.0],
-         '^', markersize=10, color='m')
-ax1.plot(portfolio.loc[signals.positions == -1.0].index,
-         portfolio.total[signals.positions == -1.0],
-         'v', markersize=10, color='k')
-
-# Show the plot
-plt.show()
-
+symbolText = 'AAPL'
+portfolio = BTA.backTest(initialCapital,signals,aapl,symbolText)
 
 '''
 Evaluatioins tools
 '''
-
-from TestingUtilities import EvaluationTools as EveT
-
 EveT.sharpeRatio(portfolio)
-
 
 '''
 Maximum Drawdown, which is used to measure the largest single drop from peak to 
