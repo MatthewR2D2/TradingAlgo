6e80bee2bdffde6b4e3398881146faedc7bbcdc6
Matthew <matthew.c.r.millar@gmail.com>
2019-02-18 15:41:08 +0900

    Updated for new infrastructure

--- TradingAlgo/VWAP.py
+++ TradingAlgo/VWAP.py
@@ -1,55 +1,60 @@
-from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
 import matplotlib.pyplot as plt
-from config import FinTechConfig as ftc
-from pprint import pprint
+import numpy as np
 
 
-# 1. open  2. high  3. low  4. close  5. volume
+class VWAP():
+    def __init__(self):
+        print("Created VWAP object")
+    '''
+    Available Columns
+    ['Open' 'High' 'Low' 'Close' 'Volume' 'Ex-Dividend' 'Split Ratio'
+     'Adj. Open' 'Adj. High' 'Adj. Low' 'Adj. Close' 'Adj. Volume'
+     'Open-Close']
+    '''
 
-def vwap(data):
-    print("Starting VWAP")
-    data['TP'] = (data['2. high'] + data['3. low'] + data['4. close'] + data['1. open']) / 4
-    data['TPV'] = data.TP * data['5. volume']
-    data['SumTPV'] = data.TPV.cumsum()
-    data['SumVol'] = data['5. volume'].cumsum()
-    data["VWAP"] = data.SumTPV.div(data.SumVol)
-    print("Finished VWAP")
-    return data
+    '''
+    This sets up the columns that are needed for VWAP
+    Call this before the VWAP function
+    '''
+    def setUpData(self, symbol):
+        # add vwap to the dataframe and other colums
+        symbol["TP"] = ""  # Typical Price
+        symbol["TPV"] = ""
+        symbol["SumTPV"] = ""
+        symbol["SumVol"] = ""
+        symbol["VWAP"] = ""
+        # Get rid of everything but VWAP
+        newColumns = ["TPV", "SumTPV", "SumVol"]
+        return symbol, newColumns
 
-def setUpData(data):
-    # add vwap to the dataframe and other colums
-    data["TP"] = ""
-    data["TPV"] = ""
-    data["SumTPV"] = ""
-    data["SumVol"] = ""
-    data["VWAP"] = ""
-    columns = ["TP", "TPV", "SumTPV", "SumVol"]
+    '''
+    This is the VWAP function 
+    '''
 
-    return data, columns
+    def vwap(self, symbol, columns, side):
+        symbol['TP'] = (symbol['High'] + symbol['Low'] + symbol['Close'] + symbol['Open']) / 4
+        symbol['TPV'] = symbol.TP * symbol['Volume']
+        symbol['SumTPV'] = symbol.TPV.cumsum()
+        symbol['SumVol'] = symbol['Volume'].cumsum()
+        symbol["VWAP"] = symbol.SumTPV.div(symbol.SumVol)
 
+        # Remove added columns from the dataframe
+        symbol.drop(columns, inplace=True, axis=1)
 
+        # Buy is 1 Sell is 0
+        symbol['VWAP-Signal'] = np.where(symbol["VWAP"] > symbol['TP'], 1.0, 0.0)
+        print(symbol[["VWAP", 'TP', 'VWAP-Signal']])
+        return symbol
 
-symbol = 'MSFT'
-interval = '1min'
-outputsize = 'full'
+    def plotVWAP(self, symbol):
 
-# Get TimeSeries
-ts = TimeSeries(key=ftc.alpha_key, output_format='pandas')
+        symbol[['VWAP', 'TP', 'VWAP-Signal']].plot()
+        plt.title("VWAP [High, Low, Close, Open]")
 
-data, meta_data = ts.get_intraday(symbol=symbol, interval=interval, outputsize=outputsize)
-
-data, columns = setUpData(data)
-
-# do vwap and twap calculation
-newData = vwap(data)
-print("Remove data columns")
-newData.drop(columns, inplace=True, axis=1)
+        plt.show()
 
 
 
-print(newData.describe())
-print("Plotting data")
-newData.plot.line()
 
-pprint(newData.head(2))
+
+
