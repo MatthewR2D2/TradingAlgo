74a93c4197a54e9033d5eccf38a71426bcf5a4a2
Matthew <matthew.c.r.millar@gmail.com>
2019-02-18 15:52:17 +0900

    init commit

---
+++ MachineLearningTest/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+# Short Description@
+# 
+# Full Description@
+# 
+__author__ = "Matthew Millar"
+__copyright__ = ""
+__credits__ =
+__license__ = ""
+__version__ = "0.0.0"
+__maintainer__ = "Matthew Millar"
+__email__ = "matthew.millar@igniterlabs.com"
+__status__ = "Dev"
+
+'''
